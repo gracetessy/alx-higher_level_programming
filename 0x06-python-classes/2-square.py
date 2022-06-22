5486618c8494056feb19a09a74f723633687cb52
gracetessy <graceabrahamimaji@gmail.com>
2022-06-22 00:33:47 +0000

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,8 +8,8 @@ class Square:
     def __init__(self, size=0):
         """Constructor of a Square with the size"""
         if (type(size) is not int):
-           raise (TypeError("size must be an integer"))
+            raise (TypeError("size must be an integer"))
         elif (size < 0):
             raise (ValueError("size must be >= 0"))
         else:
-           self.__size = size
+            self.__size = size
