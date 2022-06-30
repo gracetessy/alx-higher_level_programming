4d99dcae7785dcbc247ebdf6b5ea8f9a21b553b2
gracetessy <graceabrahamimaji@gmail.com>
2022-06-29 19:06:56 -0700

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python3
-"""This program define a class Rectangle with it attributes"""
+"""This program define a class Rectangle with private attributes and methods"""e
 
 
 class Rectangle():
     """
     A Rectangle Class with the private instance attributes width and height
-        """
+    and public methods
+    """
 
     def __init__(self, width=0, height=0):
         """
-       Constructor of the class Rectangle
-         Args:
-            - width (default = 0): int
-            - heigth (default = 0): int
-        """
+        Constructor of the class Rectangle
+          Args:
+           - width (default = 0): int
+           - heigth (default = 0): int
+       """
         self.height = height
         self.width = width
+
+    def area(self):
+        """Calculate the area of a Rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Get the perimeter of a Rectangle"""
+       if (self.__width == 0 or self.__height == 0):
+            return 0
+
+       return (self.__width * 2) + (self.__height * 2)
 
     @property
     def width(self):
@@ -26,8 +38,8 @@ class Rectangle():
     def width(self, value):
         """
         Getter of the property value
-         Args:
-           - value: int
+          Args:
+            - value: int
         """
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
@@ -35,20 +47,20 @@ class Rectangle():
         if value < 0:
             raise ValueError('width must be >= 0')
 
-        self.__width = value
+        self.__width = 
 
     @property
-    def height(self):
-        """Getter of the property height"""
-        return self.__height
+   def height(self):
+       """Getter of the property height"""
+       return self.__height
 
     @height.setter
     def height(self, value):
         """
         Getter of the property value
           Args:
-           - value: int
-        """
+            - value: int
+        """        
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
 
