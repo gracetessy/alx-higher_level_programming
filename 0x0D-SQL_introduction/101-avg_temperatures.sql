4d87b26619e5eca7dc63e4fa130d7828836c2191
gracetessy <graceabrahamimaji@gmail.com>
2022-08-10 02:08:36 +0100

    101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -2,7 +2,5 @@
 -- Query to display the average temperature by city ordered by temperature
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-WHERE month = 7 OR month = 8
 GROUP BY city
-ORDER BY avg_temp DESC
-LIMIT 3;
+ORDER BY avg_temp DESC;;
