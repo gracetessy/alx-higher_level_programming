89bfb65ef267d0a7b7bc659d3dbaa9d9456f4f5c
gracetessy <graceabrahamimaji@gmail.com>
2022-08-10 01:57:32 +0100

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,6 @@
-[200~-- Script that lists all records of a table
--- Query to lists all records of the table second_table who have name value
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+-- Lists all records of the table second_table having a name value.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
